9e8271779e2ba91e0b9e7c89edc0dcee2d2437d7
Arvindkumar7742 <arvindkumar14032004@gmail.com>
2024-11-01 11:30:34 +0530

    server to mock the response created from mock miragejs

--- roll_based_access/src/App.js
+++ roll_based_access/src/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import './App.css';
+import { createMockServer } from "./mirage"
 
 function App() {
+
+
+
+
+
+  useEffect(() => {
+    createMockServer();
+  }, []);
+
   return (
     <div className="text-center">
-        
+      
     </div>
   );
 }
